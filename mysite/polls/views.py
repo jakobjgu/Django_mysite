961aa1c0d072122d23c27b53be26102ca5e85a66
Jakob <61227884+jakobjgu@users.noreply.github.com>
2020-08-04 09:44:37 +0200

    small changes to practice PyCharm GitHub integration

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,12 +1,6 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def index(request):
-    """
-
-    :param request:
-    :return:
-    """
     return HttpResponse("Hello World. You're at the polls index.")
 # Create your views here.
