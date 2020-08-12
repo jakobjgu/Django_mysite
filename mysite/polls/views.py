a232f79eae8d5a5976783bbdf34d439bb0fbc495
Jakob <61227884+jakobjgu@users.noreply.github.com>
2020-08-12 20:17:00 +0200

    Continued the Django tutorial, fixed time_zone

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,23 @@
 from django.http import HttpResponse
-
+from .models import Question
 
 def index(request):
     return HttpResponse("Hello World. You're at the polls index.")
-# Create your views here.
+
+
+def detail(request, question_id):
+    q = Question.objects.get(pk=question_id)
+    question_txt = q.question_text
+    response_text = f"""You're looking at question {question_id}.
+                         The question you are being asked is: {question_txt}
+                      """
+    return HttpResponse(response_text)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("you're voting on question %s." % question_id)
